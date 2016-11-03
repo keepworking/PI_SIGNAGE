f0a084d7143528389a30938c017ae3d18182c6f2
yevgeny hong <vey_robotics@naver.com>
2016-11-04 00:27:00 +0900

    update new file

--- install.sh
+++ install.sh
@@ -7,7 +7,15 @@ apt-get upgrade -y
 LIST="lxde lightdm xinit nginx php5-fpm php-apc"
 apt-get install $LIST -y
 
-echo "finish install"
+echo "finish install package"
+
+wget https://raw.githubusercontent.com/keepworking/PI_SIGNAGE/master/www/index.html
+mv index.html /var/www/html/index.html
+#인덱스 파일 다운로드
+
+wget https://raw.githubusercontent.com/keepworking/PI_SIGNAGE/master/www/info.php
+mv info.php /var/www/html/info.php
+#php info 파일 다운로드
+
 echo ""
 echo "next install the chromium-borwser :)"
-
