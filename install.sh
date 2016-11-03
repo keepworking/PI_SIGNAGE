cb3f92e3a1a9d47b1e14bbd31e55f2c5ec6eca76
yevgeny hong <yev_science@naver.com>
2016-11-03 14:50:50 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ echo "start install"
 
 apt-get update
 apt-get upgrade -y
-LIST="lxde lightdm xinit chromium-borwser nginx php5-fpm php-apc"
+LIST="lxde lightdm xinit nginx php5-fpm php-apc"
 apt-get install $LIST -y
 
 echo "finish install"
+echo ""
+echo "next install the chromium-borwser :)"
+
