c026d402869b69d824e1cf7560c0b498cfcd161d
yevgeny hong <yev_science@naver.com>
2016-11-09 14:40:56 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 sleep 10
 unclutter -idle 0 &
 export DISPLAY=:0 && xset 0 dpms
-export DISPLAY=:0 && chromium-browser -kiosk 127.0.0.1 &
+export DISPLAY=:0 && chromium-browser -kiosk 127.0.0.1/index.html &
