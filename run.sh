fc18bae23583f6d03c9a5451dbcf10e85e35abf4
yevgeny hong <yev_science@naver.com>
2016-11-10 14:12:17 +0900

    update
    never turn off display

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 sleep 10
 unclutter -idle 0 &
-export DISPLAY=:0 && xset 0 dpms
-export DISPLAY=:0 && chromium-browser -kiosk 127.0.0.1 &
+export DISPLAY=:0
+xset s off
+xset -dpms
+chromium-browser -kiosk 127.0.0.1/clock.html &
