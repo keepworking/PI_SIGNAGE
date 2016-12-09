a834e123276e986af93d48e9756882803f4b74cf
yevgeny hong <vey_robotics@naver.com>
2016-12-09 14:21:32 +0900

    home

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export DISPLAY=:0
 xset s off
 xset -dpms
 xset s 0 0
-DISPLAY=:0 chromium-browser --noerrdialogs --kiosk --app=http://127.0.0.1/index.html --incognito &
+DISPLAY=:0 chromium-browser --noerrdialogs --kiosk --app=http://127.0.0.1/home.html --incognito &
